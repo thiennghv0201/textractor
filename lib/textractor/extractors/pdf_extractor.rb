34d0ef84f21afbcde5cc83c6e53492e7c38cca73
Thien Nguyen <thiennguyen@freec.asia>
2024-11-13 15:23:20 +0700

    pdftotext with raw

--- lib/textractor/extractors/pdf_extractor.rb
+++ lib/textractor/extractors/pdf_extractor.rb
@@ -1,7 +1,7 @@
 module Textractor::Extractors
   class PDFExtractor
     def text_from_path(path)
-      `pdftotext -enc UTF-8 #{Escape.shell_single_word(path)} - 2>/dev/null`
+      `pdftotext -raw -enc "UTF-8" #{Escape.shell_single_word(path)} - 2>/dev/null`
     end
   end
 end
