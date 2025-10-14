8356d48894671842afa6c8e8c0da229f5af7ed61
Thien Nguyen <thiennguyen@freec.asia>
2025-10-14 13:35:46 +0700

    update gem

--- lib/textractor/extractors/xlsx_extractor.rb
+++ lib/textractor/extractors/xlsx_extractor.rb
@@ -1,7 +1,7 @@
 module Textractor::Extractors
   class XlsxExtractor
     def text_from_path(path)
-      `in2csv -eutf-8 --blank #{Escape.shell_single_word(path)} | sed "s/,/ /g"`
+      `in2csv -eutf-8 --blank --reset-dimensions #{Escape.shell_single_word(path)} | sed "s/,/ /g"`
     end
   end
 end
